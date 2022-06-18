11d720f123bf8628142dc9fd41a00360c966e3ad
N3TH1K4 <88619598+N3TH1K4@users.noreply.github.com>
2022-06-18 20:47:04 +0530

    Update karma.py

--- wbb/modules/karma.py
+++ wbb/modules/karma.py
@@ -43,11 +43,14 @@ from wbb.utils.filter_groups import karma_negative_group, karma_positive_group
 from wbb.utils.functions import get_user_id_and_usernames
 
 __MODULE__ = "Karma"
-__HELP__ = """[UPVOTE] - Use upvote keywords like "+", "+1", "thanks" etc to upvote a message.
+__HELP__ = """
+[UPVOTE] - Use upvote keywords like "+", "+1", "thanks" etc to upvote a message.
 [DOWNVOTE] - Use downvote keywords like "-", "-1", etc to downvote a message.
 /karma_toggle [ENABLE|DISABLE] - Enable or Disable Karma System In Your Chat.
 Reply to a message with /karma to check a user's karma
-Send /karma without replying to any message to check karma list of top 10 users"""
+Send /karma without replying to any message to check karma list of top 10 users
+
+**@Goddess_of_War_Rbot**"""
 
 regex_upvote = r"^(\+|\+\+|\+1|thx|tnx|ty|thank you|thanx|thanks|pro|cool|good|👍|\+\+ .+)$"
 regex_downvote = r"^(-|--|-1|👎|-- .+)$"
