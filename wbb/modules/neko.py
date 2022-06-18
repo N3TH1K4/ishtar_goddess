e9176824f271aa0b7b720dacbe684bbec9fd6bf9
N3TH1K4 <88619598+N3TH1K4@users.noreply.github.com>
2022-06-18 16:22:20 +0530

    Update neko.py

--- wbb/modules/neko.py
+++ wbb/modules/neko.py
@@ -8,22 +8,21 @@ from wbb import SUDOERS, app
 from wbb.core.sections import section
 from wbb.utils.dbfunctions import is_gbanned_user, user_global_karma
 
-__MODULE__ = "Neko"
+__MODULE__ = "Anime Extra"
 __HELP__ = """
-/info [USERNAME|ID] - Get info about a user.
-/chat_info [USERNAME|ID] - Get info about a chat.
+/neko - Get an image of a Neko (cat girl)
+/awallpaper - Get an anime wallpaper
+/fgirl - Get an image of a fox girl (kinda looks like neko tho)
+
 """
 @app.on_message(filters.command("neko") & ~filters.edited)
 async def neko(_, message: Message):
     target = "neko"
-    await message.reply_photo(nekos.img(target))
+    if message.reply_to_message:
+        await message.reply_to_message.reply_photo(nekos.img(target))
+    else:
+        await message.reply_photo(nekos.img(target))
     
-@app.on_message(filters.command("awallpaper") & ~filters.edited)
-async def awallpaper(_, message: Message):
-    target = "wallpaper"
-    await message.reply_photo(nekos.img(target))
-    
-
 @app.on_message(filters.command("fgirl") & ~filters.edited)
 async def fox_girl(_, message: Message):
     target = "fox_girl"
@@ -31,3 +30,21 @@ async def fox_girl(_, message: Message):
         await message.reply_to_message.reply_photo(nekos.img(target))
     else:
         await message.reply_photo(nekos.img(target))
+    
+
+@app.on_message(filters.command("awallpaper") & ~filters.edited)
+async def awall(_, message: Message):
+    target = "wallpaper"
+    if message.reply_to_message:
+        await message.reply_to_message.reply_photo(nekos.img(target))
+    else:
+        await message.reply_photo(nekos.img(target))
+        
+        
+@app.on_message(filters.command("feed") & ~filters.edited)
+async def feed(_, message: Message):
+    target = "feed"
+    if message.reply_to_message:
+        await message.reply_to_message.reply_video(nekos.img(target))
+    else:
+        await message.reply_video(nekos.img(target))
