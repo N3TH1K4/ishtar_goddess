9a9a915077af5a6cc6e18bf8ca600f5f13f3f7ad
N3TH1K4 <88619598+N3TH1K4@users.noreply.github.com>
2022-06-18 15:17:25 +0530

    Update neko.py

--- wbb/modules/neko.py
+++ wbb/modules/neko.py
@@ -2,11 +2,17 @@ import os
 import nekos
 import requests
 from PIL import Image
-import os
 from pyrogram import filters
 from pyrogram.types import Message
 from wbb import SUDOERS, app
+from wbb.core.sections import section
+from wbb.utils.dbfunctions import is_gbanned_user, user_global_karma
 
+__MODULE__ = "Neko"
+__HELP__ = """
+/info [USERNAME|ID] - Get info about a user.
+/chat_info [USERNAME|ID] - Get info about a chat.
+"""
 @app.on_message(filters.command("neko") & ~filters.edited)
 async def neko(_, message: Message):
     target = "neko"
