4ffb3e60a1912ae8b08b00b5c6b7a9cc37fc1c64
N3TH1K4 <88619598+N3TH1K4@users.noreply.github.com>
2022-06-18 20:19:39 +0530

    Update blacklist.py

--- wbb/modules/blacklist.py
+++ wbb/modules/blacklist.py
@@ -43,6 +43,8 @@ __HELP__ = """
 /blacklisted - Get All The Blacklisted Words In The Chat.
 /blacklist [WORD|SENTENCE] - Blacklist A Word Or A Sentence.
 /whitelist [WORD|SENTENCE] - Whitelist A Word Or A Sentence.
+
+**@Goddess_of_War_Rbot**
 """
 
 
